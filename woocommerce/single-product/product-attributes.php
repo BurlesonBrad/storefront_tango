39df946e9fdff1e5d9f8d4dad920c4c6f42589d9
maxkom <max.kombarov@gmail.com>
2016-04-06 14:24:19 +0300

    Initial commit

---
+++ woocommerce/single-product/product-attributes.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Product attributes
+ *
+ * Used by list_attributes() in the products class.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/product-attributes.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+ * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+ * as little as possible, but it does happen. When this occurs the version of the template file will.
+ * be bumped and the readme will list any important changes.
+ *
+ * @see 	    http://docs.woothemes.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.1.3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+$has_row    = false;
+$alt        = 1;
+$attributes = $product->get_attributes();
+
+ob_start();
+
+?>
+<table class="shop_attributes">
+
+	<?php if ( $product->enable_dimensions_display() ) : ?>
+
+		<?php if ( $product->has_weight() ) : $has_row = true; ?>
+			<tr class="<?php if ( ( $alt = $alt * -1 ) === 1 ) echo 'alt'; ?>">
+				<th><?php _e( 'Weight', 'woocommerce' ) ?></th>
+				<td class="product_weight"><?php echo $product->get_weight() . ' ' . esc_attr( get_option( 'woocommerce_weight_unit' ) ); ?></td>
+			</tr>
+		<?php endif; ?>
+
+		<?php if ( $product->has_dimensions() ) : $has_row = true; ?>
+			<tr class="<?php if ( ( $alt = $alt * -1 ) === 1 ) echo 'alt'; ?>">
+				<th><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
+				<td class="product_dimensions"><?php echo $product->get_dimensions(); ?></td>
+			</tr>
+		<?php endif; ?>
+
+	<?php endif; ?>
+
+	<?php
+	//sorting attributes as required
+	$attributes["pa_kolekcia"]['position'] = '1';
+	$attributes["pa_sezon"]['position'] = '2';
+	$attributes["pa_kolir"]['position'] = '3';
+	$attributes["pa_rozmir"]['position'] = '4';
+	$attributes["pa_zov_material"]['position'] = '5';
+	$attributes["pa_vn_material"]['position'] = '6';
+	$attributes["pa_mat_pidoshvi"]['position'] = '7';
+	$attributes["pa_mat_ustilki"]['position'] = '8';
+	$attributes["pa_povnota"]['position'] = '9';
+	$attributes["pa_rozmirnist"]['position'] = '10';
+	$attributes["pa_vis_pidbora"]['position'] = '11';
+	$attributes["pa_vis_pidoshvi"]['position'] = '12';
+
+	$tmp = Array();
+	foreach( $attributes as $ma ) {
+	    $tmp[] = $ma['position'];
+	}
+
+	array_multisort( $tmp, $attributes );
+	//end of custom sorting
+	?>
+
+	<?php foreach ( $attributes as $attribute ) :
+		if ( empty( $attribute['is_visible'] ) || ( $attribute['is_taxonomy'] && ! taxonomy_exists( $attribute['name'] ) ) ) {
+			continue;
+		} else {
+			$has_row = true;
+		}
+		?>
+		<tr class="<?php if ( ( $alt = $alt * -1 ) == 1 ) echo 'alt'; ?>">
+			<th><?php echo wc_attribute_label( $attribute['name'] ); ?></th>
+			<td><?php
+				if ( $attribute['is_taxonomy'] ) {
+
+					$values = wc_get_product_terms( $product->id, $attribute['name'], array( 'fields' => 'names' ) );
+					echo apply_filters( 'woocommerce_attribute', wpautop( wptexturize( implode( ', ', $values ) ) ), $attribute, $values );
+
+				} else {
+
+					// Convert pipes to commas and display values
+					$values = array_map( 'trim', explode( WC_DELIMITER, $attribute['value'] ) );
+					echo apply_filters( 'woocommerce_attribute', wpautop( wptexturize( implode( ', ', $values ) ) ), $attribute, $values );
+
+				}
+			?></td>
+		</tr>
+	<?php endforeach; ?>
+
+</table>
+<?php
+if ( $has_row ) {
+	echo ob_get_clean();
+} else {
+	ob_end_clean();
+}
